44bc9aa7d81412bfae022c2cd4137066c4405072
Ian Maurmann <ikmxx@yahoo.com>
2024-09-28 20:18:56 -0700

    Add OxCSS 0.2.0

---
+++ src/oxcss-resource-pack/resource/oxcss-0.2.0/src/feature/event/ox-event.js
@@ -0,0 +1,51 @@
+/**
+ * @file Ox Event Listener for OxCSS
+ * @author Ian Maurmann
+ * @copyright (c) 2016-2024 - Ian Maurmann
+ * @license MIT
+ */
+
+
+
+// Check if Ox object exists
+if( typeof Ox === 'undefined' || Ox === null || Ox === false){
+    var Ox = {};
+}
+
+// __________ __________ __________ __________ __________
+
+// Check if Ox Event Listener exists
+if( typeof Ox.Event === 'undefined' || Ox.Event === null || Ox.Event === false){
+    Ox.Event = {};
+}
+
+
+
+/**
+ * Delegate a UI Event
+ * @author Ian Maurmann
+ * @param {string} selector - jQuery selector of the HTML element that will be clicked/dragger/hovered, etc.
+ * @param {string} event_type_name - The event type, i.e. "click".
+ * @param {Ox.Event~methodDelegateParamHandlerCallback} handler - Function that will be called when the event happens.
+ */
+Ox.Event.delegate = function(selector, event_type_name, handler){
+    // Start delegatable as an empty object
+    let delegatable = {};
+
+    // Add property for handling the given event type using the given handler
+    delegatable[event_type_name] = function(event){
+        var element = $(event.target);
+        handler(element, event);
+    };
+
+    // Listen for event to happen on given selector
+    $(document).on(delegatable, selector );
+}
+/**
+ * Function that will be called when the event happens.
+ * @callback Ox.Event~methodDelegateParamHandlerCallback
+ * @param {object} element - jQuery element object
+ * @param {event} event - Event that was triggered
+ */
+
+// __________ __________ __________ __________ __________
